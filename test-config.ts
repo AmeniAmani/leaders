292357b925176f82c4d09618d91f1a04ec721f5f
Sync automatique <noreply@leaders.local>
2026-09-22 14:11:08 +0000

    sync auto 2026-09-22 14:11

--- test-config.ts
+++ test-config.ts
@@ -14,3 +14,4 @@ console.log("configObj:", configObj);
 console.log("datasource keys:", Object.keys(configObj));
 console.log("datasource direct property access:", (configObj as any).datasource);
 console.log("JSON:", JSON.stringify(configObj, null, 2));
+// test de synchronisation automatique - Tue Sep 22 14:11:08 UTC 2026
